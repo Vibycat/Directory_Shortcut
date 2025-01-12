6d120d722e467f4f73a6a63c3f082ae1941b81af
Vibycat <Kylemay38@gmail.com>
2025-01-12 07:52:43 -0700

    formatted the deploy_main.py file for future use.  Moved the .exe file to main so its not ignored in the /dist folder.

--- main/deploy_main.py
+++ main/deploy_main.py
@@ -1,31 +1,62 @@
-# Use this to run multiple scripts at once 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Script Name: deploy_main.py
+Description: Script to run multiple Python scripts sequentially using subprocess.
+Author: Kyle
+Date: 2025-01-12
+Version: 1.1
+"""
 
-# Import
-
+######################################
+#              Imports              #
+######################################
 import subprocess
 
+#########################################
+#          Script Definitions          #
+#########################################
+# Define your scripts as variables
+MAIN_SCRIPT = "scripts/main_script.py"
+TESTING_SCRIPT = "scripts/testing_script.py"
+# Add filepaths to the new scripts you want to add
 
-# Define your scripts as variables: 
-
-Main_Script= "scripts/main_script.py"
-Testing_Script= "scripts/testing_script.py"
-
-# Create a function to call each script 
-
+#########################################
+#          Helper Functions           #
+#########################################
 # Script 1
-def deploy_main(Main_Script):
-    print("deploying main script.")
-    subprocess.run(["python",Main_Script])
-    print("completed main script.")
+def deploy_main(script_path):
+    """
+    Executes the main script.
+    
+    :param script_path: Path to the main script file.
+    """
+    print("Deploying main script...")
+    subprocess.run(["python", script_path])
+    print("Completed main script.")
 
-# Script 2 
-def deploy_testting(testing_script):
-    print("deploying testing script")
-    subprocess.run(["python",Testing_Script])
-    print("completed testing script")
+# Script 2
+def deploy_testing(script_path):
+    """
+    Executes the testing script.
+    
+    :param script_path: Path to the testing script file.
+    """
+    print("Deploying testing script...")
+    subprocess.run(["python", script_path])
+    print("Completed testing script.")
 
-# Run Main (call each function script:)
+#########################################
+#           Main Function             #
+#########################################
+def main(): 
+    """Main function to call each script function."""
+    deploy_main(MAIN_SCRIPT)
+    deploy_testing(TESTING_SCRIPT)
+    print("End of script!")
+
+#########################################
+#         Script Entry Point          #
+#########################################
 if __name__ == "__main__":
-    deploy_main(Main_Script)
-    deploy_testting(Testing_Script)
-    print ("end of script!")
+    main()
